f63073201d528f8d9297b3c9ccf7a83c380d7aa3
greenfrogs <5961364+greenfrogs@users.noreply.github.com>
2021-08-13 11:38:27 +0200

    add notify

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <boost/lockfree/queue.hpp>
 #include <iostream>
+#include <mutex>
+#include <condition_variable>
 #include <thread>
+#include <chrono>
 
 #define QUEUE_SIZE 5
 
 using namespace boost;
+
+std::mutex m;
+std::condition_variable cv;
 
 bool isPrime(int n) {
     for (int i = 2; i <= n / i; ++i) {
@@ -25,10 +31,12 @@ unsigned generatePrime() {
 }
 
 [[noreturn]] void fillQueueWithPrimes(boost::lockfree::queue<unsigned, boost::lockfree::capacity<QUEUE_SIZE>> *queue) {
+    std::unique_lock<std::mutex> lk(m);
+
     srand(time(nullptr));
     while (true) {
         unsigned prime = generatePrime();
-        while (queue->push(prime) == 0);
+        cv.wait(lk, [queue, prime]{return queue->push(prime);});
         std::cout << "Pushed prime: " << prime << std::endl;
     }
 }
@@ -43,11 +51,16 @@ int main() {
     auto queue = new boost::lockfree::queue<unsigned, boost::lockfree::capacity<QUEUE_SIZE>>();
     std::thread t1(fillQueueWithPrimes, queue);
     t1.detach();
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
     
     for (int i = 0; i < 150; i++) {
         unsigned prime = requestPrime(queue);
         std::cout << "Popped prime: " << prime << std::endl;
+        cv.notify_all();
     }
+
+    std::this_thread::sleep_for(std::chrono::seconds(10));
 
     return 0;
 }
